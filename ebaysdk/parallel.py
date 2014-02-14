4a0702d5f537f7626b5d7942fa9b83368834ab66
andi <andi@inventorum.com>
2014-02-14 14:58:58 +0100

    - one more unicode fix in parallel - gitignore for pycharm - version 1.0.1-dev4

--- ebaysdk/parallel.py
+++ ebaysdk/parallel.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-
 '''
 © 2012-2013 eBay Software Foundation
 Authored by: Tim Keefer
 Licensed under CDDL 1.0
 '''
 
+from __future__ import absolute_import, unicode_literals
 import grequests
 from ebaysdk.exception import ConnectionError
 
